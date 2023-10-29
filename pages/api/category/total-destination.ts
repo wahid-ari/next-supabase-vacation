7d726c7b18497e5ff844d29af4ddb9f668b28f9d
wahidari <wahiid.ari@gmail.com>
2023-10-29 08:32:49 +0700

    feat: update Category API endpoint

--- pages/api/category/total-destination.ts
+++ pages/api/category/total-destination.ts
@@ -4,27 +4,30 @@ import { supabase } from '@/libs/supabase';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
-
+  // FIX create this
   switch (method) {
     case 'GET':
-      const { data: books_genres } = await supabase.from('book_books_genres').select(`*`).order('id');
-      const { data: genres } = await supabase.from('book_genres').select(`*`).order('id');
+      const { data: vacation_categories } = await supabase
+        .from('vacation_destination_category')
+        .select(`*`)
+        .order('id');
+      const { data: categories } = await supabase.from('vacation_category').select(`*`).order('id');
       // Make an array of object structure
       let items = [];
-      for (const genre of genres) {
+      for (const category of categories) {
         items.push({
-          id: genre.id,
-          label: genre.name,
-          slug: genre.slug,
+          id: category.id,
+          label: category.name,
+          slug: category.slug,
           total: 0,
         });
       }
-      // Count total book that have same genre
+      // Count total vacation that have same category
       let result = [];
       for (const item of items) {
-        for (const book_genre of books_genres) {
-          if (book_genre.genre_id == item.id) {
-            let filtered = items.filter((i) => i.id == book_genre.genre_id)[0];
+        for (const vacation_category of vacation_categories) {
+          if (vacation_category.category_id == item.id) {
+            let filtered = items.filter((i) => i.id == vacation_category.category_id)[0];
             filtered.total += 1;
             result.push(filtered);
           }
