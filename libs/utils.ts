94ae743432df2b33e61339ef0b5d1b860fcd9f5d
wahidari <wahiid.ari@gmail.com>
2023-11-02 17:37:30 +0700

    util: create youtubeGetId func

--- libs/utils.ts
+++ libs/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// TODO Docs https://gist.github.com/takien/4077195
+// TODO DOcs https://gist.github.com/takien/4077195?permalink_comment_id=3128949#gistcomment-3128949
+export function youTubeGetID(url: string) {
+  const [a, , b] = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  if (b !== undefined) {
+    return b.split(/[^0-9a-z_-]/i)[0];
+  } else {
+    return a;
+  }
+}
