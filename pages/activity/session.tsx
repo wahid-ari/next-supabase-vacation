90f3fe13b060236d2142f9545e9da37be6208004
wahidari <wahiid.ari@gmail.com>
2023-11-13 21:35:27 +0700

    feat: update Session page, add delete to each item

--- pages/activity/session.tsx
+++ pages/activity/session.tsx
@@ -8,8 +8,9 @@ import { useSessionsData } from '@/libs/swr';
 import { cn } from '@/libs/utils';
 import useToast from '@/hooks/use-hot-toast';
 
+import { Button } from '@/components/ui/Button';
+
 import Layout from '@/components/layout/Layout';
-import Button from '@/components/systems/Button';
 import Dialog from '@/components/systems/Dialog';
 import InputDebounce from '@/components/systems/InputDebounce';
 import Shimmer from '@/components/systems/Shimmer';
@@ -23,7 +24,7 @@ export default function Session() {
   const { data, error } = useSessionsData();
   const { updateToast, pushToast } = useToast();
   const [inputDebounceValue, setInputDebounceValue] = useState('');
-  const [openDeleteAllDialog, setOpenDeleteAllDialog] = useState(false);
+  const [deleteDialog, setDeleteDialog] = useState({ dialog: false, id: null });
 
   const filteredData =
     inputDebounceValue === ''
@@ -41,9 +42,9 @@ export default function Session() {
       isLoading: true,
     });
     try {
-      const res = await axios.delete(`${process.env.NEXT_PUBLIC_API_ROUTE}/api/session`);
+      const res = await axios.delete(`${process.env.NEXT_PUBLIC_API_ROUTE}/api/session?id=${deleteDialog.id}`);
       if (res.status == 200) {
-        setOpenDeleteAllDialog(false);
+        setDeleteDialog({ dialog: false, id: null });
         updateToast({ toastId, message: res?.data?.message, isError: false });
         mutate(`${process.env.NEXT_PUBLIC_API_ROUTE}/api/session`);
         router.push('/activity/session');
@@ -71,21 +72,23 @@ export default function Session() {
     <Layout title='Session - MyVacation' description='View and Manage Session - MyVacation' prefetch={['/api/session']}>
       <div className='mb-4 flex flex-wrap items-center justify-between gap-y-3'>
         <Title>Session</Title>
-        {/* <Button.danger onClick={() => setOpenDeleteAllDialog(true)} className='flex items-center gap-2'>
-          <TrashIcon className='h-5 w-5' />
+        {/* <Button variant='destructive' onClick={() => setDeleteDialog({ dialog: true, id: null })}>
+          <TrashIcon className='mr-2 h-4 w-4' />
           Delete All
-        </Button.danger> */}
+        </Button> */}
       </div>
 
       <Dialog
-        title='Delete All Session'
-        open={openDeleteAllDialog}
+        title={`Delete ${deleteDialog.id == null ? 'All' : ''} Session`}
+        open={deleteDialog.dialog}
         isDanger
-        setOpen={setOpenDeleteAllDialog}
-        onClose={() => setOpenDeleteAllDialog(false)}
+        setOpen={() => setDeleteDialog((prev) => ({ ...prev, dialog: true }))}
+        onClose={() => setDeleteDialog((prev) => ({ ...prev, dialog: false }))}
         onConfirm={handleDeleteAll}
       >
-        <div className='mt-5 text-center sm:text-left'>Are you sure want to delete All Session ?</div>
+        <div className='mt-5 text-center sm:text-left'>
+          Are you sure want to delete {deleteDialog.id == null ? 'All' : 'This'} Session ?
+        </div>
       </Dialog>
 
       <InputDebounce
@@ -102,11 +105,12 @@ export default function Session() {
           head={
             <>
               <TableSimple.td shrink>No</TableSimple.td>
-              <TableSimple.td shrink>ID</TableSimple.td>
+              <TableSimple.td shrink>User ID</TableSimple.td>
               <TableSimple.td className={cn(filteredData.length < 1 && 'w-32 text-center')}>Name</TableSimple.td>
               <TableSimple.td>Token</TableSimple.td>
               <TableSimple.td shrink>Date</TableSimple.td>
               <TableSimple.td shrink>Time</TableSimple.td>
+              <TableSimple.td shrink>Action</TableSimple.td>
             </>
           }
           caption={filteredData.length > 0 ? '' : 'No Data'}
@@ -115,11 +119,22 @@ export default function Session() {
             return (
               <TableSimple.tr key={index}>
                 <TableSimple.td shrink>{index + 1}</TableSimple.td>
-                <TableSimple.td>{item.user_id}</TableSimple.td>
+                <TableSimple.td className='text-center'>{item.user_id}</TableSimple.td>
                 <TableSimple.td>{item.vacation_user.name}</TableSimple.td>
                 <TableSimple.td>{item.token.split('.')[2]}</TableSimple.td>
                 <TableSimple.td>{item.created_at.split('T')[0]}</TableSimple.td>
                 <TableSimple.td>{getTime(item.created_at)}</TableSimple.td>
+                <TableSimple.td>
+                  <Button
+                    title={`Delete ${item.vacation_user.name}`}
+                    size='sm'
+                    variant='destructive'
+                    className='px-2'
+                    onClick={() => setDeleteDialog({ dialog: true, id: item.id })}
+                  >
+                    <TrashIcon className='h-4 w-4' />
+                  </Button>
+                </TableSimple.td>
               </TableSimple.tr>
             );
           })}
