b4b9a962ae76e781090f0be09f864706b7d86a26
wahidari <wahiid.ari@gmail.com>
2023-11-21 15:32:51 +0700

    fix: API endpoint response

--- libs/supabase.ts
+++ libs/supabase.ts
@@ -4,10 +4,10 @@ import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(process.env.SUPABASE_URL || '', process.env.SUPABASE_ANON_KEY || '');
 
 export async function getSessionToken(res: NextApiResponse, header: string, token: string) {
-  if (!header) return res.status(401).json({ error: 'Please provide bearer token in headers' });
+  if (!header) return res.status(401).json({ message: 'Please provide bearer token in headers' });
   const { data } = await supabase.from('vacation_session').select('*').eq('token', token).single();
   if (data) return data;
-  else res.status(401).json({ error: 'Token invalid' });
+  else res.status(401).json({ message: 'Token invalid' });
 }
 
 export async function writeLogs(user_id: number, action: string, table: string = '', data_id: string | string[] = '') {
