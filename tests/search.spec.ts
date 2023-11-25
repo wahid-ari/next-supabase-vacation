fa33e913876f4b7c725b71f5aecb72cb7fadc46c
wahidari <wahiid.ari@gmail.com>
2023-11-25 11:46:54 +0700

    feat: create test for Search page

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -10,170 +10,163 @@ test.describe('Testing Search Page', () => {
     await expect(page).toHaveTitle(/Search/);
     await expect(page.getByRole('heading', { name: 'Search' })).toBeVisible();
   });
-  test('should show search result of "jane" using enter keyboard', async ({ page }) => {
-    const searchInput = page.getByPlaceholder('Search Title, Author, ISBN');
-    await searchInput.fill('jane');
+  test('should show search result of "bali" using enter keyboard', async ({ page }) => {
+    const searchInput = page.getByPlaceholder('Search Destination, Video, Province, Category and Island');
+    await searchInput.fill('bali');
     await searchInput.press('Enter');
-    await expect(page.getByText(/Searching “jane”.../)).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
+    await expect(page.getByText(/Searching “bali”.../)).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
   });
-  test('should show search result of "game" using search button', async ({ page }) => {
-    const searchInput = page.getByPlaceholder('Search Title, Author, ISBN');
+  test('should show search result of "toba" using search button', async ({ page }) => {
+    const searchInput = page.getByPlaceholder('Search Destination, Video, Province, Category and Island');
     const searchButton = page.getByRole('button', { name: 'Search' });
-    await searchInput.fill('game');
+    await searchInput.fill('toba');
     await searchButton.click();
-    await expect(page.getByText(/Searching “game”.../)).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('The Hunger Games (The Hunger Games #1)')).toBeVisible();
+    await expect(page.getByText(/Searching “toba”.../)).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'hero Lake Toba' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'The Heartbeat of Toba' })).toBeVisible();
   });
   test('should show search history after visit setting page and back to search page', async ({ page }) => {
-    // first visit to search page, and search "jane"
-    const searchInput = page.getByPlaceholder('Search Title, Author, ISBN');
-    await searchInput.fill('jane');
+    // first visit to search page, and search "bali"
+    const searchInput = page.getByPlaceholder('Search Destination, Video, Province, Category and Island');
+    await searchInput.fill('bali');
     await searchInput.press('Enter');
-    await expect(page.getByText(/Searching “jane”.../)).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
+    await expect(page.getByText(/Searching “bali”.../)).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
     // go to setting page and back to search page
     await page.getByRole('link', { name: 'Setting' }).click();
     await page.getByRole('link', { name: 'Search' }).click();
     // show search history
     await expect(page.getByText('Recent Search')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
   });
   test('should remove all search history', async ({ page }) => {
-    // first visit to search page, and search "jane"
-    const searchInput = page.getByPlaceholder('Search Title, Author, ISBN');
-    await searchInput.fill('jane');
+    // first visit to search page, and search "bali"
+    const searchInput = page.getByPlaceholder('Search Destination, Video, Province, Category and Island');
+    await searchInput.fill('bali');
     await searchInput.press('Enter');
-    await expect(page.getByText(/Searching “jane”.../)).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
+    await expect(page.getByText(/Searching “bali”.../)).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
     // go to setting page and back to search page
     await page.getByRole('link', { name: 'Setting' }).click();
     await page.getByRole('link', { name: 'Search' }).click();
     // show search history
     await expect(page.getByText('Recent Search')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
     // clear all search history
     await page.getByRole('button', { name: 'Clear All' }).click();
     await expect(page.getByText('Recent Search')).not.toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).not.toBeVisible();
-    await expect(page.getByText('Jane Eyre')).not.toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).not.toBeVisible();
-    await expect(page.getByText('Jane Austen')).not.toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).not.toBeVisible();
+    await expect(page.getByText('West Bali National Park')).not.toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).not.toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).not.toBeVisible();
   });
-  test('should remove all books search history', async ({ page }) => {
-    // first visit to search page, and search "jane"
-    const searchInput = page.getByPlaceholder('Search Title, Author, ISBN');
-    await searchInput.fill('jane');
+  test('should remove all destination search history', async ({ page }) => {
+    // first visit to search page, and search "bali"
+    const searchInput = page.getByPlaceholder('Search Destination, Video, Province, Category and Island');
+    await searchInput.fill('bali');
     await searchInput.press('Enter');
-    await expect(page.getByText(/Searching “jane”.../)).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
+    await expect(page.getByText(/Searching “bali”.../)).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
     // go to setting page and back to search page
     await page.getByRole('link', { name: 'Setting' }).click();
     await page.getByRole('link', { name: 'Search' }).click();
     // show search history
     await expect(page.getByText('Recent Search')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
-    // clear books search history
-    await page.getByRole('button', { name: 'Clear Books' }).click();
-    await expect(page.getByText('Recent Search')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).not.toBeVisible();
-    await expect(page.getByText('Jane Eyre')).not.toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
+    // clear destination search history
+    await page.getByRole('button', { name: 'Clear Destination' }).click();
+    await expect(page.getByRole('heading', { name: 'Destination' })).not.toBeVisible();
+    await expect(page.getByText('West Bali National Park')).not.toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
   });
-  test('should remove all authors search history', async ({ page }) => {
-    // first visit to search page, and search "jane"
-    const searchInput = page.getByPlaceholder('Search Title, Author, ISBN');
-    await searchInput.fill('jane');
+  test('should remove all video search history', async ({ page }) => {
+    // first visit to search page, and search "bali"
+    const searchInput = page.getByPlaceholder('Search Destination, Video, Province, Category and Island');
+    await searchInput.fill('bali');
     await searchInput.press('Enter');
-    await expect(page.getByText(/Searching “jane”.../)).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
+    await expect(page.getByText(/Searching “bali”.../)).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
     // go to setting page and back to search page
     await page.getByRole('link', { name: 'Setting' }).click();
     await page.getByRole('link', { name: 'Search' }).click();
     // show search history
     await expect(page.getByText('Recent Search')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
-    // clear authors search history
-    await page.getByRole('button', { name: 'Clear Authors' }).click();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).toBeVisible();
+    // clear video search history
+    await page.getByRole('button', { name: 'Clear Video' }).click();
     await expect(page.getByText('Recent Search')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Jane Eyre')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).not.toBeVisible();
-    await expect(page.getByText('Jane Austen')).not.toBeVisible();
-  });
-  test('should remove single author search history', async ({ page }) => {
-    // first visit to search page, and search "jane"
-    const searchInput = page.getByPlaceholder('Search Title, Author, ISBN');
-    await searchInput.fill('jane');
-    await searchInput.press('Enter');
-    await expect(page.getByText(/Searching “jane”.../)).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
-    // go to setting page and back to search page
-    await page.getByRole('link', { name: 'Setting' }).click();
-    await page.getByRole('link', { name: 'Search' }).click();
-    // show search history
-    await expect(page.getByText('Recent Search')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Authors' })).toBeVisible();
-    await expect(page.getByText('Jane Austen')).toBeVisible();
-    // clear single authors search history
-    await page
-      .locator('div')
-      .filter({ hasText: /^Jane AustenWebX$/ })
-      .getByRole('button', { name: 'X' })
-      .click();
-    await expect(page.getByText('Jane Austen')).not.toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).not.toBeVisible();
+    await expect(page.getByRole('button', { name: 'Bali' })).not.toBeVisible();
   });
   test('should remove single destination search history', async ({ page }) => {
-    // first visit to search page, and search "jane"
-    const searchInput = page.getByPlaceholder('Search Title, Author, ISBN');
-    await searchInput.fill('hunger');
+    // first visit to search page, and search "bali"
+    const searchInput = page.getByPlaceholder('Search Destination, Video, Province, Category and Island');
+    await searchInput.fill('bali');
     await searchInput.press('Enter');
-    await expect(page.getByText(/Searching “hunger”.../)).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Mockingjay (The Hunger Games #3)')).toBeVisible();
+    await expect(page.getByText(/Searching “bali”.../)).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
     // go to setting page and back to search page
     await page.getByRole('link', { name: 'Setting' }).click();
     await page.getByRole('link', { name: 'Search' }).click();
     // show search history
     await expect(page.getByText('Recent Search')).toBeVisible();
-    await expect(page.getByRole('heading', { name: 'Books' })).toBeVisible();
-    await expect(page.getByText('Mockingjay (The Hunger Games #3)')).toBeVisible();
-    // clear single authors search history
-    await page
-      .locator('div')
-      .filter({ hasText: /^Mockingjay \(The Hunger Games #3\)2010X$/ })
-      .getByRole('button', { name: 'X' })
-      .click();
-    await expect(page.getByText('Mockingjay (The Hunger Games #3)')).not.toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Destination' })).toBeVisible();
+    await expect(page.getByText('West Bali National Park')).toBeVisible();
+    // clear single destination search history
+    await page.getByTitle('Delete West Bali National Park').click();
+    await expect(page.getByText('West Bali National Park')).not.toBeVisible();
+  });
+  test('should remove single video search history', async ({ page }) => {
+    // first visit to search page, and search "toba"
+    const searchInput = page.getByPlaceholder('Search Destination, Video, Province, Category and Island');
+    await searchInput.fill('toba');
+    await searchInput.press('Enter');
+    await expect(page.getByText(/Searching “toba”.../)).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'The Heartbeat of Toba' })).toBeVisible();
+    // go to setting page and back to search page
+    await page.getByRole('link', { name: 'Setting' }).click();
+    await page.getByRole('link', { name: 'Search' }).click();
+    // show search history
+    await expect(page.getByText('Recent Search')).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Video' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'The Heartbeat of Toba' })).toBeVisible();
+    // clear single video search history
+    await page.getByTitle('Delete The Heartbeat of Toba').click();
+    await expect(page.getByRole('button', { name: 'The Heartbeat of Toba' })).not.toBeVisible();
   });
 });
