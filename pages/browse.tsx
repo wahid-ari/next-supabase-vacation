3be5503d1b2f52a10333c4d3453dd06a3391fa72
wahidari <wahiid.ari@gmail.com>
2023-12-06 11:59:30 +0700

    feat: update Browse page

--- pages/browse.tsx
+++ pages/browse.tsx
@@ -8,14 +8,14 @@ import { compareSearchResult, useSearchHistory } from '@/store/use-search-histor
 import { useSearchData } from '@/libs/swr';
 import { youTubeGetID } from '@/libs/utils';
 
+import { Button } from '@/components/ui/Button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/Dialog';
+import { Input } from '@/components/ui/Input';
 
 import DestinationCardItem from '@/components/dashboard/DestinationCardItem';
 import VideoCardItem from '@/components/dashboard/VideoCardItem';
 import FrontLayout from '@/components/front/FrontLayout';
-import Button from '@/components/systems/Button';
 import Heading from '@/components/systems/Heading';
-import LabeledInput from '@/components/systems/LabeledInput';
 import Shimmer from '@/components/systems/Shimmer';
 import Text from '@/components/systems/Text';
 import Title from '@/components/systems/Title';
@@ -91,13 +91,12 @@ export default function Browse() {
   return (
     <FrontLayout title='Browse - MyVacation' description='Browse destination - MyVacation'>
       <div className='pt-2'>
-        <Title>Browse</Title>
+        <Title>Search</Title>
       </div>
 
       <form className='mt-4' onSubmit={handleSubmit}>
         <div className='flex items-end gap-2'>
-          <LabeledInput
-            wrapperClassName='w-full'
+          <Input
             name='search'
             placeholder='Search Destination, Video, Province, Category and Island'
             type='text'
@@ -105,7 +104,7 @@ export default function Browse() {
             onChange={(e) => setQuery(e.target.value)}
             required
           />
-          <Button type='submit' value='Submit' className='mb-4 !px-8 !py-2.5'>
+          <Button type='submit' value='Submit' size='lg'>
             Search
           </Button>
         </div>
@@ -115,7 +114,7 @@ export default function Browse() {
         <>
           {!data && (
             <>
-              <Text>Searching &#8220;{search}&#8221;...</Text>
+              <Text className='pt-6'>Searching &#8220;{search}&#8221;...</Text>
               <Heading h3 className='mt-6'>
                 Destination
               </Heading>
@@ -146,7 +145,7 @@ export default function Browse() {
           )}
 
           {data?.destination?.length < 1 && data?.video?.length < 1 ? (
-            <div className='mb-12 mt-8 rounded border border-red-500 p-3'>
+            <div className='my-12 rounded border border-red-500 p-3'>
               <p className='text-red-500'>{`No results for "${query || search}"`}</p>
             </div>
           ) : null}
@@ -160,7 +159,7 @@ export default function Browse() {
                 {data?.destination?.map((item: any, index: number) => (
                   <div key={index} className='relative'>
                     <DestinationCardItem
-                      href={`/destination/detail/${item.id}`}
+                      href={`/destinations/${item.slug}`}
                       image_url={item.image_url}
                       name={item.name}
                     />
@@ -193,8 +192,8 @@ export default function Browse() {
         <>
           {searchHistory.destination.length > 0 || searchHistory.video.length > 0 ? (
             <>
-              <div className='mt-6 flex items-center justify-between'>
-                <Heading h2 className='!mb-0 !text-[20px]'>
+              <div className='mt-8 flex items-center justify-between'>
+                <Heading h2 className='!mb-0 !text-[22px]'>
                   Recent Search
                 </Heading>
                 <button
@@ -222,7 +221,7 @@ export default function Browse() {
                     {searchHistory.destination?.map((item: any, index: number) => (
                       <div key={index} className='relative'>
                         <DestinationCardItem
-                          href={`/destination/detail/${item.id}`}
+                          href={`/destinations/${item.slug}`}
                           image_url={item.image_url}
                           name={item.name}
                         />
@@ -286,7 +285,7 @@ export default function Browse() {
         </>
       )}
 
-      <Heading h3 className='mt-8 !text-[19px]'>
+      <Heading h3 className='mt-8 !text-[20px]'>
         Browse
       </Heading>
       <div className='mt-2 grid grid-cols-1 gap-6 min-[480px]:grid-cols-2 min-[700px]:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
