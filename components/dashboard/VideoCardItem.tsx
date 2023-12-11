1501cc44820da88288846d8eca4b3232a794e629
wahidari <wahiid.ari@gmail.com>
2023-12-11 14:17:02 +0700

    feat: update VideoCardItem focus

--- components/dashboard/VideoCardItem.tsx
+++ components/dashboard/VideoCardItem.tsx
@@ -45,7 +45,13 @@ export default function VideoCardItem({
         )}
       >
         <div className='flex justify-center items-center h-full'>
-          <svg className='h-12 w-12 sm:h-14 sm:w-14' height='100%' version='1.1' viewBox='0 0 68 48' width='100%'>
+          <svg
+            className='h-12 w-12 sm:h-14 sm:w-14 group-focus-visible:outline-none group-focus-visible:ring-2 group-focus-visible:ring-sky-500 rounded'
+            height='100%'
+            version='1.1'
+            viewBox='0 0 68 48'
+            width='100%'
+          >
             <path
               d='M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z'
               fill='#f00'
