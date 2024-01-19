2f1cad5caa74e0679dcf931479fd31dd70566e6b
wahidari <wahiid.ari@gmail.com>
2024-01-19 14:40:46 +0700

    feat: update DestinationCardItem component

--- components/card/DestinationCardItem.tsx
+++ components/card/DestinationCardItem.tsx
@@ -6,6 +6,7 @@ import { ImageIcon } from 'lucide-react';
 import { cn } from '@/libs/utils';
 
 type Props = {
+  className?: string;
   href: string;
   name: string;
   image_url: string;
@@ -13,6 +14,7 @@ type Props = {
 };
 
 export default function DestinationCardItem({
+  className,
   href = '/destination',
   name = 'Destination Name',
   image_url,
@@ -27,7 +29,7 @@ export default function DestinationCardItem({
         className='group overflow-hidden rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500'
       >
         {image_url ? (
-          <div className='relative h-64 overflow-hidden'>
+          <div className={cn('relative h-64 overflow-hidden', className)}>
             <Image
               className={cn(
                 'w-full transform object-cover brightness-90 transition-all duration-500 group-hover:scale-110 group-hover:brightness-100',
@@ -47,7 +49,7 @@ export default function DestinationCardItem({
         )}
         <div className='absolute inset-0 rounded-md bg-gradient-to-b from-transparent via-transparent to-neutral-950'>
           <div className='flex h-full items-end justify-center'>
-            <p className='mb-4 line-clamp-2 px-4 text-center text-base font-medium text-white'>{name}</p>
+            <p className='mb-4 line-clamp-2 px-2.5 text-center text-base font-medium text-white sm:px-4'>{name}</p>
           </div>
         </div>
       </Link>
